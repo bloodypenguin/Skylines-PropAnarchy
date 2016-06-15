842801d5d31a9ca2ce5c5c563bbdcedd701b9304
Egor Aralov <aei.omsk@gmail.com>
2016-06-15 18:09:48 +0600

    Fix issue in Always On mode that caused IndexOutOfBounds exception in Surface Painter on loading (and probably in other mods)

--- PropAnarchy/Detours/PropInstanceDetour.cs
+++ PropAnarchy/Detours/PropInstanceDetour.cs
@@ -1,4 +1,5 @@
-﻿using ColossalFramework;
+﻿using System.Runtime.CompilerServices;
+using ColossalFramework;
 using ColossalFramework.Math;
 using PropAnarchy.OptionsFramework;
 using PropAnarchy.Redirection;
@@ -10,6 +11,7 @@ namespace PropAnarchy.Detours
     public class PropInstanceDetour
     {
         [RedirectMethod]
+        [MethodImpl(MethodImplOptions.NoInlining)]
         public static void set_Blocked(ref PropInstance prop, bool value)
         {
             if (!value)
@@ -17,13 +19,40 @@ namespace PropAnarchy.Detours
         }
 
         [RedirectMethod]
-        private static void CheckOverlap(ref PropInstance prop, ushort propID)
+        private static void CheckOverlap(ref PropInstance prop, ushort propID) //this method reproduces original code for most part to prevent exceptions
         {
+            PropInfo info = prop.Info;
+            if (info == null)
+                return;
+            ItemClass.CollisionType collisionType = ((int)prop.m_flags & 32) != 0 ? ItemClass.CollisionType.Elevated : ItemClass.CollisionType.Terrain;
+            Randomizer randomizer = new Randomizer((int)propID);
+            float num1 = info.m_minScale + (float)((double)randomizer.Int32(10000U) * ((double)info.m_maxScale - (double)info.m_minScale) * 9.99999974737875E-05);
+            float num2 = info.m_generatedInfo.m_size.y * num1;
+            Vector3 position = prop.Position;
+            float minY = position.y;
+            float maxY = position.y + num2;
+            float num3 = !prop.Single ? 4.5f : 0.5f;
+            Quad2 quad = new Quad2();
+            Vector2 vector2 = VectorUtils.XZ(position);
+            quad.a = vector2 + new Vector2(-num3, -num3);
+            quad.b = vector2 + new Vector2(-num3, num3);
+            quad.c = vector2 + new Vector2(num3, num3);
+            quad.d = vector2 + new Vector2(num3, -num3);
+            bool flag = false;
+            if (info.m_class != null)
+            {
+                if (Singleton<NetManager>.instance.OverlapQuad(quad, minY, maxY, collisionType, info.m_class.m_layer, (ushort)0, (ushort)0, (ushort)0))
+                    flag = true;
+                if (Singleton<BuildingManager>.instance.OverlapQuad(quad, minY, maxY, collisionType, info.m_class.m_layer, (ushort)0, (ushort)0, (ushort)0))
+                    flag = true;
+            }
+            //begin mod
             if (!OptionsWrapper<Options>.Options.anarchyAlwaysOn)
             {
                 return;
             }
             prop.Blocked = false;
+            //end mod
         }
     }
 }
--- PropAnarchy/Detours/TreeInstanceDetour.cs
+++ PropAnarchy/Detours/TreeInstanceDetour.cs
@@ -1,4 +1,7 @@
-﻿using PropAnarchy.OptionsFramework;
+﻿using System.Runtime.CompilerServices;
+using ColossalFramework;
+using ColossalFramework.Math;
+using PropAnarchy.OptionsFramework;
 using PropAnarchy.Redirection;
 using UnityEngine;
 
@@ -8,6 +11,7 @@ namespace PropAnarchy.Detours
     public class TreeInstanceDetour
     {
         [RedirectMethod]
+        [MethodImpl(MethodImplOptions.NoInlining)]
         public static void set_GrowState(ref TreeInstance tree, int value)
         {
             if (value == 0)
@@ -18,13 +22,39 @@ namespace PropAnarchy.Detours
         }
 
         [RedirectMethod]
-        private static void CheckOverlap(ref TreeInstance tree, uint treeID)
+        private static void CheckOverlap(ref TreeInstance tree, uint treeID) //this method reproduces original code for most part to prevent exceptions
         {
+            TreeInfo info = tree.Info;
+            if (info == null)
+                return;
+            ItemClass.CollisionType collisionType = ((int)tree.m_flags & 32) != 0 ? ItemClass.CollisionType.Elevated : ItemClass.CollisionType.Terrain;
+            Randomizer randomizer = new Randomizer(treeID);
+            float num1 = info.m_minScale + (float)((double)randomizer.Int32(10000U) * ((double)info.m_maxScale - (double)info.m_minScale) * 9.99999974737875E-05);
+            float num2 = info.m_generatedInfo.m_size.y * num1;
+            Vector3 position = tree.Position;
+            float minY = position.y;
+            float maxY = position.y + num2;
+            float num3 = !tree.Single ? 4.5f : 0.5f;
+            Quad2 quad = new Quad2();
+            Vector2 vector2 = VectorUtils.XZ(position);
+            quad.a = vector2 + new Vector2(-num3, -num3);
+            quad.b = vector2 + new Vector2(-num3, num3);
+            quad.c = vector2 + new Vector2(num3, num3);
+            quad.d = vector2 + new Vector2(num3, -num3);
+            bool flag = false;
+            if (Singleton<NetManager>.instance.OverlapQuad(quad, minY, maxY, collisionType, info.m_class.m_layer, (ushort)0, (ushort)0, (ushort)0))
+                flag = true;
+            if (Singleton<BuildingManager>.instance.OverlapQuad(quad, minY, maxY, collisionType, info.m_class.m_layer, (ushort)0, (ushort)0, (ushort)0))
+                flag = true;
+            //begin mod
             if (!OptionsWrapper<Options>.Options.anarchyAlwaysOn)
             {
                 return;
             }
+            if (tree.GrowState != 0)
+                return;
             tree.GrowState = 1;
+            //end mod
         }
     }
 }
